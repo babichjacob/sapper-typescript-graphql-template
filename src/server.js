c4d69dcaa20770e2226e28f944646ea70ec7924d
Jacob Babich <jacobbabichpublic+git@gmail.com>
2020-05-17 17:11:29 -0400

    🎨 style: simplify crufty GraphQL server creation leftover from the opinionated project base

--- src/server.js
+++ src/server.js
@@ -1,28 +1,33 @@
 import * as sapper from "@sapper/server";
 import compression from "compression";
+import express from "express";
 import sirv from "sirv";
-import { createApolloServerExpress } from "./graphql/index.ts";
+import { createApolloServer } from "./graphql/index.ts";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
 
-const createSapperAndApolloServer = async () => {
-	const app = await createApolloServerExpress();
+const createSapperAndApolloServer = async (graphqlPath) => {
+	const app = express();
+
+	const apolloServer = await createApolloServer();
+
+	apolloServer.applyMiddleware({ app, path: graphqlPath });
 
 	app.use(
 		compression({ threshold: 0 }),
-		sirv("static", { dev: true }),
+		sirv("static", { dev }),
 		sapper.middleware(),
 	);
 
 	return app;
 };
 
-createSapperAndApolloServer().then((app) => {
+createSapperAndApolloServer("/graphql").then((app) => {
 	app.listen(PORT, (err) => {
 		if (err) console.log("error", err);
 	});
 });
 
-export { createSapperAndApolloServer, sapper };
+export { sapper };
